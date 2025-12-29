3254148ecd5b75099b9833f8c1c034e9789ab314
uptobe8 <ia.uptobe@gmail.com>
2025-12-29 13:25:58 +0100

    Remove output:export basePath configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/My-Meeting2Slides',
   typescript: {
     ignoreBuildErrors: true,
-
-    images{:     
+  },
+  images:{
     unoptimized: true,
-  },
-  },
   },
 }
 
